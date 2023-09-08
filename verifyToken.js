21548d79729fc0d25e211b448a6970a4aa853524
thinh <thinhbui2911@gmail.com>
2023-09-07 17:53:34 -0700

    test

--- verifyToken.js
+++ verifyToken.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 export const verifyToken = (req,res,next) =>{
     const token = req.cookies.access_token;
+    console.log("token",token);
     if(!token) return next(createError(401,"Not Authenticated"));
     jwt.verify(token, process.env.JWT,(err,user)=>{
         if(err) return next(createError(403,"Not Valid Token"));
